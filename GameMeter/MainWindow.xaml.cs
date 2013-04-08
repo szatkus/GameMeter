3f418560b857527b254e2ac35961a8e25b34ee4a
szatkus <szatkus@gmail.com>
2013-04-09 00:40:28 +0200

    First commit

---
+++ GameMeter/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace GameMeter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProcessMeter meter = new ProcessMeter();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            meter.Changed += meter_Changed;
+        }
+
+        private void choose_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Executables|*.exe";
+            if (dialog.ShowDialog() == true)
+            {
+                meter.FileName = dialog.FileName;
+                System.Windows.Controls.Button runButton = (System.Windows.Controls.Button)FindName("runButton");
+                runButton.IsEnabled = true;
+                System.Windows.Controls.Image thumbnail = (System.Windows.Controls.Image) FindName("icon");
+                thumbnail.Source = null;
+                System.Drawing.Icon ico = System.Drawing.Icon.ExtractAssociatedIcon(meter.FileName);
+                MemoryStream stream = new MemoryStream();
+                ico.Save(stream);
+                thumbnail.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(ico.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+
+        private void run_Click(object sender, RoutedEventArgs e)
+        {
+            meter.Run();
+            ((System.Windows.Controls.Button)sender).IsEnabled = false;
+            ((System.Windows.Controls.Button)FindName("chooseButton")).IsEnabled = false;
+        }
+
+        private void meter_Changed(object sender, MeterEventArgs e)
+        {
+            List<Polyline> lines = new List<Polyline>();
+            
+            this.Dispatcher.BeginInvoke((Action)delegate
+            {
+                if (!e.Finish)
+                {
+                    Canvas chart = ((System.Windows.Controls.Canvas)FindName("chart"));
+                    chart.Children.Clear();
+                    Rectangle bg = new Rectangle();
+                    bg.Width = chart.Width;
+                    bg.Height = chart.Height;
+                    bg.Stroke =  System.Windows.Media.Brushes.Black;
+                    bg.Fill = System.Windows.Media.Brushes.White;
+                    chart.Children.Add(bg);
+                    foreach (Plot plot in e.Plots.Values)
+                    {
+                        Polyline poly = new Polyline();
+                        poly.StrokeThickness = 2;
+                        poly.Stroke = new SolidColorBrush(plot.color);
+                        int i = 0;
+                        foreach (int value in plot.Values)
+                        {
+                            //Console.WriteLine(i);
+                            int x = (int)((i * chart.ActualWidth) / plot.Values.Count);
+                            int y = (int)((value * chart.ActualHeight) / meter.Scale);
+                            //Console.WriteLine(x);
+                            //Console.WriteLine(y);
+                            Point point = new Point(x, y);
+                            poly.Points.Add(point);
+                            i++;
+                        }
+                        chart.Children.Add(poly);
+                    }
+
+                    Panel stats = ((System.Windows.Controls.Panel)FindName("stats"));
+                    stats.Children.Clear();
+                    for (int i = 0; i < meter.thresholds.Length; i++)
+                    {
+                        Label label = new Label();
+                        label.Content = meter.thresholds[i] + "%: " + meter.winners[i];
+                        label.Height = 28;
+                        label.Width = stats.ActualWidth / meter.thresholds.Length;
+                        stats.Children.Add(label);
+                    }
+                }
+                else
+                {
+                    ((System.Windows.Controls.Button)FindName("chooseButton")).IsEnabled = true;
+                    ((System.Windows.Controls.Button)FindName("runButton")).IsEnabled = true;
+                }
+                
+            });
+            
+        }
+
+    }
+}
---
+++ GameMeter/ProcessMeter.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Timers;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace GameMeter
+{
+
+    public class MeterEventArgs : EventArgs
+    {
+        public Dictionary<int, Plot> Plots;
+        public bool Finish;
+    }
+
+    public class Plot
+    {
+        public Queue<int> Values = new Queue<int>();
+        public Color color;
+        public int Best = -1;
+        static Random random = new Random();
+        public Plot()
+        {
+            color = Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
+            for (int i = 0; i < 100; i++)
+            {
+                Values.Enqueue(0);
+            }
+        }
+    }
+
+    public delegate void ChangedEventHandler(object sender, MeterEventArgs e);
+
+    class ProcessMeter
+    {
+        public String FileName;
+        public event ChangedEventHandler Changed;
+        private Dictionary<int, Plot> Plots;
+        private Process Process;
+        public int RefreshInterval = 1000;
+        public int Scale = 100;
+        Dictionary<int, TimeSpan> times = new Dictionary<int, TimeSpan>();
+        MeterEventArgs args = new MeterEventArgs();
+        Timer timer;
+        public int[] thresholds = new int[] { 0, 5, 10, 25, 50, 75, 90 };
+        public int[] winners;
+
+        public ProcessMeter()
+        {
+            winners = new int[thresholds.Length];
+        }
+
+        internal void Run()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = FileName;
+            startInfo.WorkingDirectory = FileName.Substring(0, FileName.LastIndexOf("\\"));
+            Process = Process.Start(startInfo);
+            args.Finish = false;
+            Plots = new Dictionary<int, Plot>();
+            args.Plots = Plots;
+            timer = new Timer(RefreshInterval);
+            timer.Elapsed += OnTimedEvent;
+            timer.Start();
+            for (int i = 0; i < winners.Length; i++)
+            {
+                winners[i] = 0;
+            }
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if (!Process.HasExited)
+            {
+                Process.Refresh();
+                foreach (ProcessThread thread in Process.Threads)
+                {
+                    try
+                    {
+                        if (!Plots.ContainsKey(thread.Id))
+                        {
+                            Plots[thread.Id] = new Plot();
+                        }
+                        if (times.ContainsKey(thread.Id))
+                        {
+                            Plot plot = Plots[thread.Id];
+                            double delta = (thread.TotalProcessorTime - times[thread.Id]).TotalMilliseconds;
+                            int usage = (int)Math.Ceiling((delta / RefreshInterval) * Scale);
+                            plot.Values.Dequeue();
+                            plot.Values.Enqueue(usage);
+                            if (usage > plot.Best)
+                            {
+                                for (int i = 0; i < thresholds.Length; i++)
+                                {
+                                    int threshold = thresholds[i];
+                                    if (usage >= threshold && plot.Best < threshold)
+                                    {
+                                        winners[i]++;
+                                    }
+                                }
+                                plot.Best = usage;
+                            }
+                        }
+
+                        times[thread.Id] = thread.TotalProcessorTime;
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            else
+            {
+                args.Finish = true;
+                timer.Stop();
+            }
+            Changed(this, args);
+        }
+
+    }
+}
